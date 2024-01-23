5b8d5e7e50e0c9d9ccfc5923f2578c88cfaf8846
naty13naty <nttsegai@ucdavis.edu>
2024-01-23 09:30:30 -0800

    tasks

--- quiz1.sh
+++ quiz1.sh
@@ -1,3 +1,14 @@
 echo Natnael Tsegai
 echo $USER
 
+cd ~/Code/MCB185/data
+gunzip -c dictionary.gz | grep -E "a" | grep -E "^[cfmouta]{3,}$"
+gunzip -c dictionary.gz | grep -E "b" | grep -E "^[trnliab]{3,}$"
+gunzip -c dictionary.gz | grep -E "c" | grep -E "^[amdinoac]{3,}$"
+gunzip -c dictionary.gz | grep -E "z" | grep -E "^[naigroaz]{3,}$"
+
+cd ~/Code/MCB185/data
+gunzip -c dictionary.gz | grep -E "a" | grep -E "^[cfmouta]{3,}$" | wc
+gunzip -c dictionary.gz | grep -E "b" | grep -E "^[trnliab]{3,}$" | wc
+gunzip -c dictionary.gz | grep -E "c" | grep -E "^[amdinoac]{3,}$" | wc
+gunzip -c dictionary.gz | grep -E "z" | grep -E "^[naigroaz]{3,}$" | wc
