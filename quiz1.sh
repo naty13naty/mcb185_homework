2e3491c4813348a6fee6d57af7ffd21b76962227
naty13naty <nttsegai@ucdavis.edu>
2024-01-23 09:33:55 -0800

    tasks

--- quiz1.sh
+++ quiz1.sh
@@ -1,4 +1,4 @@
-echo Natnael Tsegai # Khalid Saif
+echo Natnael Tsegai # Khalid Saifgit
 echo $USER
 
 cd ~/Code/MCB185/data
@@ -9,6 +9,6 @@ gunzip -c dictionary.gz | grep -E "z" | grep -E "^[naigroaz]{3,}$"
 
 cd ~/Code/MCB185/data
 gunzip -c dictionary.gz | grep -E "a" | grep -E "^[cfmouta]{3,}$" | wc
-gunzip -c dictionargit git ay.gz | grep -E "b" | grep -E "^[trnliab]{3,}$" | wc
+gunzip -c dictionaray.gz | grep -E "b" | grep -E "^[trnliab]{3,}$" | wc
 gunzip -c dictionary.gz | grep -E "c" | grep -E "^[amdinoac]{3,}$" | wc
 gunzip -c dictionary.gz | grep -E "z" | grep -E "^[naigroaz]{3,}$" | wc
