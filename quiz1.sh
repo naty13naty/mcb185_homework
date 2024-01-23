dd88163492f1e716af7a30d93597aaa9128064b2
naty13naty <nttsegai@ucdavis.edu>
2024-01-23 09:34:35 -0800

    tasks

--- quiz1.sh
+++ quiz1.sh
@@ -9,6 +9,6 @@ gunzip -c dictionary.gz | grep -E "z" | grep -E "^[naigroaz]{3,}$"
 
 cd ~/Code/MCB185/data
 gunzip -c dictionary.gz | grep -E "a" | grep -E "^[cfmouta]{3,}$" | wc
-gunzip -c dictionaray.gz | grep -E "b" | grep -E "^[trnliab]{3,}$" | wc
+gunzip -c dictionary.gz | grep -E "b" | grep -E "^[trnliab]{3,}$" | wc
 gunzip -c dictionary.gz | grep -E "c" | grep -E "^[amdinoac]{3,}$" | wc
 gunzip -c dictionary.gz | grep -E "z" | grep -E "^[naigroaz]{3,}$" | wc
