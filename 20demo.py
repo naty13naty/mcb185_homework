a44d24e40a36c95ccdd378932fdbf12d22451c59
naty13naty <nttsegai@ucdavis.edu>
2024-01-26 00:21:10 -0800

    practice

--- 20demo.py
+++ 20demo.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 def invert_number(n):
 	n = (n * -1)
@@ -49,3 +50,55 @@ def mid_bn_pts(x1, y1, x2, y2):
 	midway = ((x1 + x2)/2, (y1 + y2)/2)
 	return midway
 print (mid_bn_pts(2, 6, 4, 8))
+
+def is_integer(n): 
+	if n == n // 1: 
+		return 'integer'
+	else:
+		return 'not integer'
+print(is_integer(8.5))
+
+def odd_number(n):
+	if n % 2 != 0: 
+		return 'odd number'
+	elif n % 2 == 0: 
+		return 'even number'
+	
+print(odd_number(3))
+print(odd_number(8))
+
+def valid_prob(n):	
+#valid probability is a number between 0 and 1, and the sum of the number equals 1	
+	if 0 < n <= 1: 
+		return 'valid probablity'
+	else:
+		return 'not valid probability'
+print(valid_prob(0.5))
+print(valid_prob(-0.2))
+
+def molecular_wt_dna(letter): 	#letters: A, C, G, T)
+	if letter == 'A':
+		return 331.2
+	if letter == 'C': 
+		return 307.2
+	if letter == 'G':
+		return 347.2
+	if letter == 'T': 
+		return 322.2
+	
+	else:
+		return 'none'
+print(molecular_wt_dna('T'))
+
+def dna_complement(base_pair):		#base pairs: A==T, C==G
+	if base_pair == 'A':
+		return 'T'
+	if base_pair == 'C':
+		return 'G'
+	if base_pair == 'T':
+		return 'A'
+	if base_pair == 'G':
+		return 'C'
+	
+print(dna_complement('C'))
+
