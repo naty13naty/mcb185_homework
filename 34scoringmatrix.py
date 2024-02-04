5ec470ff339e24c6eb25a6cafe4ca9eeed2919dc
naty13naty <nttsegai@ucdavis.edu>
2024-02-04 10:34:42 -0800

    homework

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -1,6 +1,6 @@
-# scoring matrix by Natnael Tsegai
+# scoring matrix 
 
-nts = 'ACGT'										#Horizontal header
+nts = 'ACGT'										
 print('   ', end='')
 for i in nts:
 	print(i, end='  ')
@@ -9,6 +9,8 @@ print()
 for nt1 in nts:
 	print(nt1, end=' ')
 	for nt2 in nts:
-		if nt1 == nt2: print('+1', end=' ')
-		else 		 : print('-1', end=' ')
+		if nt1 == nt2: 
+			print('+1', end=' ')
+		else: 
+			print('-1', end=' ')
 	print()
