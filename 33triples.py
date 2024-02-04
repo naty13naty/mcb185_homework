61cb58a8c1feb88526203427c6c6e07ef8d6200d
naty13naty <nttsegai@ucdavis.edu>
2024-02-04 10:49:16 -0800

    homework

--- 33triples.py
+++ 33triples.py
@@ -3,6 +3,6 @@
 for a in range(1, 100):
 	for b in range(a, 100):
 		c = (a**2 + b**2)**0.5
-		if a == a // 1 and b == b // 1 and c == c // 1:	print('triples', a, b, c)
+		if c == c // 1:	print('triples', a, b, c)
 				
 	
