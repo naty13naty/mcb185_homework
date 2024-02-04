8d130e901006c79b092ea17e17fc9a6290527ad1
naty13naty <nttsegai@ucdavis.edu>
2024-02-04 11:00:02 -0800

    homework

--- 37nilakantha.py
+++ 37nilakantha.py
@@ -4,9 +4,9 @@ def nilakantha(n):
 	initial = 3
 	sign = 1
 	for i in range(1, n + 1):
-		formula = 4 / ((2*n + 3)**3 - (2*n + 3))
+		formula = 4 / ((2*n) * (2*n - 1) * (2*n - 2))
 		result = initial + (formula * sign)
 		sign = -1 * sign
 	return result
-print(nilakantha(1000))	
+print(nilakantha(10000))	
 	
