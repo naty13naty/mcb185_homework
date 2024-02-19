1a0edf4529c22e56f141368c1a84d5354a6d435a
naty13naty <nttsegai@ucdavis.edu>
2024-02-18 17:22:02 -0800

    homework

--- 57birthday.py
+++ 57birthday.py
@@ -1,29 +1,3 @@
-import sys
-import random
-"""
-trials = int(sys.argv[1])
-days = int(sys.argv[2])
-people = int(sys.argv[3])
-
-
-success = 0
-for i in range(trials):
-	birthdays = [random.randint(1, days) for j in range(calendar)]
-	shared_birthday = False
-	
-	for n in range(len(birthdays) - 1):
-		birthday = birthdays[n]
-		if birthday in birthdays[n+1:]:
-			shared_birthday = True
-			break
-			
-	if shared_birthday:
-			success += 1
-			
-probability = (success / trials) * 100
-print(probability)	
-"""
-
 import random
 import sys
 
