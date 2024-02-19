f6f13c9e861e4275220eb23e9628d95d4632f669
naty13naty <nttsegai@ucdavis.edu>
2024-02-18 21:25:53 -0800

    homework

--- 57birthday.py
+++ 57birthday.py
@@ -8,16 +8,15 @@ people = int(sys.argv[3])
 success = 0
 
 for i in range(trials):
-    calendar = []
+calendar = []
 
-    for j in range(people):
-        day = random.randint(0, days - 1)
+for j in range(people): day = random.randint(0, days - 1)
 
-        if day in calendar:
-            success += 1
-            break
+	if day in calendar: success += 1
+	
+	break
 
-        calendar.append(day)
+calendar.append(day)
 
 probability = success / trials * 100
 print(probability)
